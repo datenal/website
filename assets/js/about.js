0624a12045b59209ec5022a14a082218caeb5a19
datenal <datenal.tech.github@gmail.com>
2026-08-03 11:11:40 +0200

    Create about.js

---
+++ assets/js/about.js
@@ -0,0 +1,21 @@
+const menuToggle = document.getElementById("menuToggle");
+const mainNav = document.getElementById("mainNav");
+menuToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+  const icon = menuToggle.querySelector("i");
+  if (mainNav.classList.contains("active")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+  } else {
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  }
+});
+document.querySelectorAll("nav a").forEach(function(link) {
+  link.addEventListener("click", function() {
+    mainNav.classList.remove("active");
+    const icon = menuToggle.querySelector("i");
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  });
+});
